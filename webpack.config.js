2c3abd2270129764bfc9ba88306b31b71cd89747
Dariusz <dariusz.pastor@grupawp.pl>
2019-05-19 19:42:32 +0200

    gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'index.bundle.js'
+    filename: '[name].bundle.js'
   },
   mode: process.env.NODE_ENV || 'development',
   resolve: {
